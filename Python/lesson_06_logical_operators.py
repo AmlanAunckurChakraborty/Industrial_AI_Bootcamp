81d7d8c9a12ae6fe416e454eda06a19a434d732f
Amlan Aunckur <aunckuramlan@gmail.com>
2026-08-03 02:16:34 +0600

    logical operators

--- Python/lesson_06_logical_operators.py
+++ Python/lesson_06_logical_operators.py
@@ -2,13 +2,12 @@
 
 suction_pressure = int(input("Enter current suction pressure:"))
 discharge_pressure = float(input("Enter current discharge pressure:"))
-final_pressure = 67
+final_pressure = int(input("Enter current final pressure:"))
 
-print("\nSUCTION PRESSURE IS EQuAL TO DISCHARGE PRESSURE:",suction_pressure == discharge_pressure)
-print("\nSUCTINN PRESSURE IS NOT EQuAL TO DISCHARGE PRESSURE:",suction_pressure != discharge_pressure)
-print("\nSUCTION PRESSURE IS GREATER THEN DISCHARGE PRESSURE:",suction_pressure > discharge_pressure)
-print("\nSUCTION PRESSURE IS LESS THEN DISCHARGE PRESSURE:",suction_pressure < discharge_pressure)
-print("\nSUCTION PRESSURE IS GREATER THEN EQUAL TO DISCHARGE PRESSURE:",suction_pressure >= discharge_pressure)
-print("\nSUCTION PRESSURE IS LESS THEN EQUAL DISCHARGE PRESSURE:",suction_pressure <= discharge_pressure)
+print("\nSUCTION PRESSURE IS EQUAL TO DISCHARGE PRESSURE and SUCTINN PRESSURE IS NOT EQUAL TO Final PRESSURE:",suction_pressure == discharge_pressure and suction_pressure != final_pressure)
+print("\nSUCTION PRESSURE IS GREATER THEN DISCHARGE PRESSURE and SUCTINN PRESSURE IS LESS THEN Final PRESSURE:",suction_pressure > discharge_pressure and suction_pressure < final_pressure)
+print("\nSUCTION PRESSURE IS GREATER THEN DISCHARGE PRESSURE and SUCTINN PRESSURE IS LESS THEN Final PRESSURE:",suction_pressure > discharge_pressure or suction_pressure < final_pressure)
+print("\nSUCTION PRESSURE IS LESS THEN EQUAL TO DISCHARGE PRESSURE and SUCTINN PRESSURE IS GREATER THEN EQUAL TO Final PRESSURE:",suction_pressure <= discharge_pressure and suction_pressure >= final_pressure)
+print("\nSUCTION PRESSURE IS NOT LESS THEN EQUAL DISCHARGE PRESSURE:",not suction_pressure <= discharge_pressure)
 
 # so we can also compare between integer and float in python
