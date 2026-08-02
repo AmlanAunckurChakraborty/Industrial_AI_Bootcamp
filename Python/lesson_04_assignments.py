9dbf498443d35470c5a091e0d022cb17a409b624
Amlan Aunckur <aunckuramlan@gmail.com>
2026-08-03 01:08:05 +0600

    delete function

---
+++ Python/lesson_04_assignments.py
@@ -0,0 +1,29 @@
+# Lesson 02 - Variables & Datatype
+
+pressure = 950        # kPa
+temperature = float(68)      # °C
+flow_rate = 45.7      # MMSCFD
+run_status = True
+unit_name = "A"
+
+print("Compressor Outlet Pressure:", pressure, "kPa")
+print("Gas Temperature:", temperature, "°C")
+print("Gas Flow Rate:", flow_rate, "MMSCFD")
+print("Run Status:", run_status)
+print("Unit_name:",unit_name)
+
+print()
+
+print("pressure data type: ", type(pressure))
+print("temperature data type: ", type(temperature))
+print("flow rate data type: ", type(flow_rate))
+print("run_status data type: ", type(run_status))
+print("Unit Name data type: ", type(unit_name))
+
+print()
+
+print("pressure data type is int: ", isinstance(pressure,int))
+print("temperature data type is float: ", isinstance(temperature,float))
+print("flow rate data type is float: ", isinstance(flow_rate,float))
+print("run_status data type is boolen: ", isinstance(run_status,bool))
+print("Unit Name data type string: ", isinstance(unit_name,str))
