9473470836ff144f24a764d6ed27db93f3d730d3
Amlan Aunckur <aunckuramlan@gmail.com>
2026-08-03 01:58:52 +0600

    comperission

---
+++ Python/lesson_05_comparison_operators.py
@@ -0,0 +1,13 @@
+#Comparison Operators
+
+suction_pressure = int(input("Enter current suction pressure:"))
+discharge_pressure = float(input("Enter current discharge pressure:"))\
+
+print("\nSUCTINON PRESSURE IS EQAL TO DISCHARGE PRESSURE:",suction_pressure == discharge_pressure)
+print("\nSUCTINON PRESSURE IS NOT EQAL TO DISCHARGE PRESSURE:",suction_pressure != discharge_pressure)
+print("\nSUCTINON PRESSURE IS GREATER THEN DISCHARGE PRESSURE:",suction_pressure > discharge_pressure)
+print("\nSUCTINON PRESSURE IS LESS THEN DISCHARGE PRESSURE:",suction_pressure < discharge_pressure)
+print("\nSUCTINON PRESSURE IS GREATER THEN EQUAL TO DISCHARGE PRESSURE:",suction_pressure >= discharge_pressure)
+print("\nSUCTINON PRESSURE IS LESS THEN EQUAL DISCHARGE PRESSURE:",suction_pressure <= discharge_pressure)
+
+# so we can also compare between integer and float in python
