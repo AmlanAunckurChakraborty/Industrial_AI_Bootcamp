eb07e685dc523dc5dbb7edbbb7d5b6844617384f
Amlan Aunckur <aunckuramlan@gmail.com>
2026-08-03 02:00:41 +0600

    Assignment operator

--- Python/lesson_05_comparison_operators.py
+++ Python/lesson_05_comparison_operators.py
@@ -1,7 +1,7 @@
 #Comparison Operators
 
 suction_pressure = int(input("Enter current suction pressure:"))
-discharge_pressure = float(input("Enter current discharge pressure:"))\
+discharge_pressure = float(input("Enter current discharge pressure:"))
 
 print("\nSUCTINON PRESSURE IS EQAL TO DISCHARGE PRESSURE:",suction_pressure == discharge_pressure)
 print("\nSUCTINON PRESSURE IS NOT EQAL TO DISCHARGE PRESSURE:",suction_pressure != discharge_pressure)
